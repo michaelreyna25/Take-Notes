83e33c7ccd1886ff23418dca57b1020d779bba43
Michael Reyna <25.27.reyna@gmail.com>
2023-01-30 00:24:50 -0700

    added notesroute.js

---
+++ routes/notesRoute.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express 
+
+router.get('/', (req, res) => {
+    console.log(`${req.protocol}added to notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.post('/', (req, res) => {
+    console.log(`${req.protocol}added to notes`);
+    
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = { title, text };
+        readAndAppend(newNote, './db/db.json');
+        const response = JSON.stringify(newNote);
+        res.json(response);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const inquirer = require('inquirer');
 const path = require('path');
+const util = require('util');
 
 const app = express();
 const PORT = 3001
