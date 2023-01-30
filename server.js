dbf20606ccb4084fbc90a81118ceefcfb8d62ca3
Michael Reyna <25.27.reyna@gmail.com>
2023-01-30 00:38:03 -0700

    finished files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const inquirer = require('inquirer');
 const path = require('path');
 const util = require('util');
+const notes = require('./router/notes');
 
 const app = express();
 const PORT = 3001
@@ -10,7 +11,11 @@ const PORT = 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use
+app.use('/notes', notes);
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
