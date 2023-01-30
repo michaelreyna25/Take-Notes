23a87513e0790ea46d3eadd47ae2a9bf392c4065
Michael Reyna <25.27.reyna@gmail.com>
2023-01-29 23:37:28 -0700

    created server with required attachments

---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const inquirer = require('inquirer');
+const path = require('path');
+
+const app = express();
+const PORT = 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+
+);
