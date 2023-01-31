88fd3aa4f37e792d4fdf7a6356b869c0df1313f1
Michael Reyna <25.27.reyna@gmail.com>
2023-01-30 23:32:57 -0700

    fixed api call

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const express = require('express');
 const inquirer = require('inquirer');
 const path = require('path');
-const notes = require('./routes/notes.js');
+const api = require('./routes/index.js');
 
 const app = express();
+
 const PORT = 3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-app.use('/notes', notes);
+app.use('/api', api);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
